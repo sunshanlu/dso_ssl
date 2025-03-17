df95d17dee92bf05538b014e26e13e52d11f7e81
lucky-lu <ssl2001@126.com>
2025-03-17 21:54:02 +0800

    完善目录结构

--- tests/TestProject.cc
+++ tests/TestProject.cc
@@ -10,6 +10,7 @@
 
 #include "dso/Pattern.hpp"
 #include "utils/Project.hpp"
+#include "utils/TimerWrapper.hpp"
 
 using namespace dso_ssl;
 using namespace project;
@@ -19,6 +20,8 @@ void PrintVector2f(Eigen::Vector2f vector2f) { std::cout << vector2f[0] << " " <
 
 int main(int argc, char **argv)
 {
+    timer::TimerWrapper time_wrapper("Test Project");
+
     float fx = 640, fy = 640, cx = 320, cy = 230, dpi = 0.1;
 
     Eigen::AngleAxisf rji(M_PI / 8, Eigen::Vector3f(0, 0, 1));
@@ -30,15 +33,29 @@ int main(int argc, char **argv)
     Vector3fArray pj_temp_positions_not_sse(4);
     Vector2fArray pixel_not_sse(4);
 
-    for (int idx = 0; idx < 4; ++idx)
+    auto not_sse_operator = [&]()
     {
-        pixel_positions[idx] = pixel_position + Pattern::pattern8[idx];
-        Pixel2Pixel(Tji, pixel_positions[idx], dpi, fx, fy, cx, cy, pj_temp_positions_not_sse[idx], pixel_not_sse[idx]);
-    }
+        for (int idx = 0; idx < 4; ++idx)
+        {
+            pixel_positions[idx] = pixel_position + Pattern::pattern8[idx];
+            Pixel2Pixel(Tji, pixel_positions[idx], dpi, fx, fy, cx, cy, pj_temp_positions_not_sse[idx], pixel_not_sse[idx]);
+        }
+        return 0;
+    };
+
+    time_wrapper.ExecuteAndMeasure("No SSE", not_sse_operator);
 
     Vector2fArray pixel_sse;
     Vector3fArray pj_temp_positions_sse;
-    Pixel2PixelSSE(Tji, pixel_positions, dpi, fx, fy, cx, cy, pj_temp_positions_sse, pixel_sse);
+
+    auto sse_operator = [&]()
+    {
+        Pixel2PixelSSE(Tji, pixel_positions, dpi, fx, fy, cx, cy, pj_temp_positions_sse, pixel_sse);
+        return 0;
+    };
+    time_wrapper.ExecuteAndMeasure("SSE", sse_operator);
+
+    time_wrapper.TimerShow();
 
     for (int idx = 0; idx < 4; ++idx)
     {
